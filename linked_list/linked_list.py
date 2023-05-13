879251ad5accc7540a5c3b9974a852aaea6516c5
DarkStars0 <ahmadjehadalanati1@gmail.com>
2023-05-13 19:53:05 +0000

    linked list

---
+++ linked_list/linked_list.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self,value,_next=None,_back=None):
+        self.value = value
+        self.next = _next
+
+class LinkedList:
+    def __init__(self,head=None):
+        self.head = head
+
+    def insert(self,node):
+        pass
+
+    def includes(self,value):
+        pass
+
+    def __str__(self):
+        pass
